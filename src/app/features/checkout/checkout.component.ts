bf3dd776e15379457079f8fbf95290449181ca6b
Carlos Perales <carlosperales050@gmail.com>
2024-09-25 19:42:19 -0600

    add: config routes

--- src/app/features/checkout/checkout.component.ts
+++ src/app/features/checkout/checkout.component.ts
@@ -5,8 +5,6 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './checkout.component.html',
-  styleUrl: './checkout.component.css'
+  styleUrl: './checkout.component.css',
 })
-export class CheckoutComponent {
-
-}
+export default class CheckoutComponent {}
