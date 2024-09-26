3f97e5208b41d3fd023f81dc75eaf7865e17c399
Carlos Perales <carlosperales050@gmail.com>
2024-09-26 17:41:26 -0600

    add: maquetacion product detail

--- src/app/features/products/details/details.component.ts
+++ src/app/features/products/details/details.component.ts
@@ -1,10 +1,15 @@
 import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [],
+  imports: [FontAwesomeModule],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css',
 })
-export default class DetailsComponent {}
+export default class DetailsComponent {
+  faStar = faStar;
+  faHeart = faHeart;
+}
