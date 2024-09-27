1fd797b86be1d7bf765662ec7b1ab01bd6d5ba7f
Carlos Perales <carlosperales050@gmail.com>
2024-09-27 16:08:27 -0600

    emitir datos del componente hijo a padre

--- src/app/features/products/card/card.component.ts
+++ src/app/features/products/card/card.component.ts
@@ -1,13 +1,18 @@
 import { SlicePipe } from '@angular/common';
-import { Component, input } from '@angular/core';
+import { Component, EventEmitter, input, Output } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faArrowRight, faEye } from '@fortawesome/free-solid-svg-icons';
+import {
+  faArrowRight,
+  faCartShopping,
+  faEye,
+} from '@fortawesome/free-solid-svg-icons';
 import { Product } from '@shared/models/product.interface';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [FontAwesomeModule, SlicePipe],
+  imports: [FontAwesomeModule, SlicePipe, RouterLink],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css',
 })
@@ -15,7 +20,14 @@ export class CardComponent {
   // obtenemos el producto desde el componente padre
   // es una signal
   product = input.required<Product>();
+  // mandar datos al padre
+  @Output() addToCartEvent = new EventEmitter<Product>();
 
   faEye = faEye;
   faArrowRight = faArrowRight;
+  faCartShopping = faCartShopping;
+
+  AddToCart(): void {
+    this.addToCartEvent.emit(this.product());
+  }
 }
