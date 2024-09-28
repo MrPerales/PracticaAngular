1a53edd362fd60c9128c5a73dde653281c2930f3
Carlos Perales <carlosperales050@gmail.com>
2024-09-27 18:37:49 -0600

    refactor: function getProductById

--- src/app/api/product.service.ts
+++ src/app/api/product.service.ts
@@ -1,14 +1,23 @@
 import { environment } from '@envs/environment.development';
 import { HttpClient } from '@angular/common/http';
-import { Injectable, signal } from '@angular/core';
+import {
+  EnvironmentInjector,
+  Injectable,
+  runInInjectionContext,
+  signal,
+} from '@angular/core';
 import { Product } from '@shared/models/product.interface';
 import { tap } from 'rxjs';
+import { toSignal } from '@angular/core/rxjs-interop';
 
 @Injectable({
   providedIn: 'root',
 })
 export class ProductService {
-  constructor(private http: HttpClient) {
+  constructor(
+    private http: HttpClient,
+    private environmentInjector: EnvironmentInjector
+  ) {
     this.getProducts();
   }
   apiUrl = environment.API_URL;
@@ -21,7 +30,11 @@ export class ProductService {
       .subscribe();
   }
 
+  // nota : toSignal es para comvertir un observable a una signal
   getProductById(id: number) {
-    return this.http.get<Product>(`${this.apiUrl}/${id}`);
+    // retorna una signal para poder usarla
+    return runInInjectionContext(this.environmentInjector, () =>
+      toSignal<Product>(this.http.get<Product>(`${this.apiUrl}/products/${id}`))
+    );
   }
 }
