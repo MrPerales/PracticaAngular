3e82e2ff56f18f34ef38e107c2cc15aba7c810da
Carlos Perales <carlosperales050@gmail.com>
2024-09-27 18:53:15 -0600

    obteniendo un solo producto y renderizandolo

--- src/app/features/products/details/details.component.ts
+++ src/app/features/products/details/details.component.ts
@@ -1,3 +1,4 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component, input, OnInit, Signal } from '@angular/core';
 import { ProductService } from '@api/product.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
@@ -7,15 +8,24 @@ import { Product } from '@shared/models/product.interface';
 @Component({
   selector: 'app-details',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, CurrencyPipe],
   templateUrl: './details.component.html',
   styleUrl: './details.component.css',
 })
-export default class DetailsComponent {
+export default class DetailsComponent implements OnInit {
+  constructor(private productService: ProductService) {}
+
   //obtenemos valor que nos manda el componente hijo
   //nota => alias es :id del router
   productId = input<number>(0, { alias: 'id' }); // esto es una signal
+  //nota => tipamos como Signal ya que va a guardar una signal
+  //en este caso la que viene de productService
+  product!: Signal<Product | undefined>;
 
   faStar = faStar;
   faHeart = faHeart;
+
+  ngOnInit(): void {
+    this.product = this.productService.getProductById(this.productId());
+  }
 }
