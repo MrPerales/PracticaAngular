d051f635b9207917dc7db56368641704d757e5a9
Carlos Perales <carlosperales050@gmail.com>
2024-09-27 20:09:58 -0600

    add: rating dinamico

--- src/app/features/products/details/details.component.ts
+++ src/app/features/products/details/details.component.ts
@@ -2,7 +2,11 @@ import { CurrencyPipe } from '@angular/common';
 import { Component, input, OnInit, Signal } from '@angular/core';
 import { ProductService } from '@api/product.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
+import {
+  faHeart,
+  faStar,
+  faStarHalfStroke,
+} from '@fortawesome/free-solid-svg-icons';
 import { Product } from '@shared/models/product.interface';
 
 @Component({
@@ -21,9 +25,16 @@ export default class DetailsComponent implements OnInit {
   //nota => tipamos como Signal ya que va a guardar una signal
   //en este caso la que viene de productService
   product!: Signal<Product | undefined>;
+  starArray: number[] = new Array(5).fill(0);
 
   faStar = faStar;
   faHeart = faHeart;
+  faStarHalfStroke = faStarHalfStroke;
+
+  generateStarRate(): number {
+    const rate = this.product()?.rating.rate as number;
+    return Math.floor(rate);
+  }
 
   ngOnInit(): void {
     this.product = this.productService.getProductById(this.productId());
