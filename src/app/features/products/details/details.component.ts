39943dc6a731febaf09fbe3df79b881e409208fa
Carlos Perales <carlosperales050@gmail.com>
2024-09-27 18:40:12 -0600

    obtenemos id de la url

--- src/app/features/products/details/details.component.ts
+++ src/app/features/products/details/details.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, input, OnInit, Signal } from '@angular/core';
+import { ProductService } from '@api/product.service';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
+import { Product } from '@shared/models/product.interface';
 
 @Component({
   selector: 'app-details',
@@ -10,6 +12,10 @@ import { faHeart, faStar } from '@fortawesome/free-solid-svg-icons';
   styleUrl: './details.component.css',
 })
 export default class DetailsComponent {
+  //obtenemos valor que nos manda el componente hijo
+  //nota => alias es :id del router
+  productId = input<number>(0, { alias: 'id' }); // esto es una signal
+
   faStar = faStar;
   faHeart = faHeart;
 }
