3b15f74adf7ef98df63abcb9032fd282cc092518
Carlos Perales <carlosperales050@gmail.com>
2024-09-30 18:34:12 -0600

    add: animation car

--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,17 +1,20 @@
-import { Component } from '@angular/core';
+import { NgClass } from '@angular/common';
+import { Component, signal } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCartShopping, faShop } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FontAwesomeModule],
+  imports: [FontAwesomeModule, RouterLink, NgClass],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
 export class HeaderComponent {
   constructor() {}
 
+  showCart = signal<boolean>(false);
   fashop = faShop;
   faCartShopping = faCartShopping;
 }
