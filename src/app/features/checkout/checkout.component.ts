a3943c9717445a81f790e0d6e68e4b22a6249e5c
Carlos Perales <carlosperales050@gmail.com>
2024-10-03 18:16:39 -0600

    checkout dinamico

--- src/app/features/checkout/checkout.component.ts
+++ src/app/features/checkout/checkout.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { CommonModule, CurrencyPipe } from '@angular/common';
+import { Component, inject } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { faCircleXmark } from '@fortawesome/free-solid-svg-icons';
+import { CartStoreSignal } from '@shared/store/shopping.cart.store';
 
 @Component({
   selector: 'app-checkout',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe, FontAwesomeModule],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.css',
 })
-export default class CheckoutComponent {}
+export default class CheckoutComponent {
+  faCircleXMark = faCircleXmark;
+  cartStore = inject(CartStoreSignal);
+}
