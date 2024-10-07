d11a1432a934aa6397cfc08e69875ed29cd7151e
Carlos Perales <carlosperales050@gmail.com>
2024-10-07 16:54:19 -0600

    add: toastr

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,9 +3,13 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
+import { provideToastr } from 'ngx-toastr';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
 export const appConfig: ApplicationConfig = {
   providers: [
+    provideAnimations(),
+    provideToastr({ timeOut: 900, preventDuplicates: true }),
     provideRouter(routes, withComponentInputBinding()),
     provideHttpClient(withFetch()),
   ],
--- src/app/shared/store/shopping.cart.store.ts
+++ src/app/shared/store/shopping.cart.store.ts
@@ -1,4 +1,4 @@
-import { computed } from '@angular/core';
+import { computed, inject } from '@angular/core';
 import {
   patchState,
   signalStore,
@@ -8,6 +8,7 @@ import {
 } from '@ngrx/signals';
 import { CartStore } from '@shared/models/cart.store.inteface';
 import { Product } from '@shared/models/product.interface';
+import { ToastrService } from 'ngx-toastr';
 
 const initialState: CartStore = {
   products: [],
@@ -24,35 +25,45 @@ export const CartStoreSignal = signalStore(
     totalAmount: computed(() => calculateTotalAmount(products())),
   })),
   // metodos para el cart
-  withMethods(({ products, ...store }) => ({
-    addToCart(product: Product) {
-      // ya se agrego ese producto
-      const isProductInCart = products().find((item) => item.id === product.id);
-      if (isProductInCart) {
-        isProductInCart.qty++;
-        isProductInCart.subTotal = isProductInCart.qty * isProductInCart.price;
-        patchState(store, { products: [...products()] });
-      } else {
-        patchState(store, { products: [...products(), product] });
-      }
-    },
-    updateQuantityProduct(id: number, count: number) {
-      const product = products().find((item) => item.id === id);
-      if (product) {
-        product.qty = product.qty + count;
-        patchState(store, { products: [...products()] });
-      }
-    },
-    removeFromCart(id: number) {
-      const updatedProducts = products().filter((product) => product.id !== id);
-      patchState(store, { products: updatedProducts });
-    },
-    clearCart() {
-      // para no manipular el store usamos patchState
-      // (valor actual, nuevo valor )
-      patchState(store, initialState);
-    },
-  }))
+  withMethods(
+    ({ products, ...store }, toastrService = inject(ToastrService)) => ({
+      addToCart(product: Product) {
+        // ya se agrego ese producto
+        const isProductInCart = products().find(
+          (item) => item.id === product.id
+        );
+        if (isProductInCart) {
+          isProductInCart.qty++;
+          isProductInCart.subTotal =
+            isProductInCart.qty * isProductInCart.price;
+          patchState(store, { products: [...products()] });
+        } else {
+          patchState(store, { products: [...products(), product] });
+        }
+        toastrService.success('Product added', 'Store');
+      },
+      updateQuantityProduct(id: number, count: number) {
+        const product = products().find((item) => item.id === id);
+        if (product) {
+          product.qty = product.qty + count;
+          patchState(store, { products: [...products()] });
+        }
+      },
+      removeFromCart(id: number) {
+        const updatedProducts = products().filter(
+          (product) => product.id !== id
+        );
+        patchState(store, { products: updatedProducts });
+        toastrService.info('Product removed', 'Store');
+      },
+      clearCart() {
+        // para no manipular el store usamos patchState
+        // (valor actual, nuevo valor )
+        patchState(store, initialState);
+        toastrService.info('Cart cleared', 'Store');
+      },
+    })
+  )
 );
 function calculateTotalAmount(products: Product[]): number {
   return products.reduce(
