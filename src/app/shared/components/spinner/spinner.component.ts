09da2dffa8af8c7fc52170d485ace890e9a6b949
Carlos Perales <carlosperales050@gmail.com>
2024-10-07 18:39:04 -0600

    add: spinner maquetacion

---
+++ src/app/shared/components/spinner/spinner.component.ts
@@ -0,0 +1,14 @@
+import { Component } from '@angular/core';
+import { SpinnerService } from '@shared/services/spinner.service';
+
+@Component({
+  selector: 'app-spinner',
+  standalone: true,
+  imports: [],
+  templateUrl: './spinner.component.html',
+  styleUrl: './spinner.component.css',
+})
+export class SpinnerComponent {
+  constructor(private spinerService: SpinnerService) {}
+  isLoadin = this.spinerService.isLoading;
+}
