9470ed8af9032a6af6788b91f389994c944f4bc6
Carlos Perales <carlosperales050@gmail.com>
2024-10-09 15:58:40 -0600

    feat: add new cart

--- src/app/features/cart/cart/cart.component.ts
+++ src/app/features/cart/cart/cart.component.ts
@@ -1,4 +1,4 @@
-import { CurrencyPipe } from '@angular/common';
+import { CurrencyPipe, SlicePipe } from '@angular/common';
 import { Component, inject, input } from '@angular/core';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faMinus, faPlus } from '@fortawesome/free-solid-svg-icons';
@@ -8,12 +8,23 @@ import { CartStoreSignal } from '@shared/store/shopping.cart.store';
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [FontAwesomeModule, CurrencyPipe],
+  imports: [FontAwesomeModule, CurrencyPipe, SlicePipe],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.css',
 })
 export class CartComponent {
+  cartStore = inject(CartStoreSignal);
+  product = input.required<Product>();
+
   faPlus = faPlus;
   faMinus = faMinus;
-  cartStore = inject(CartStoreSignal);
+
+  plusItem(id: number) {
+    const countPlus: number = 1;
+    this.cartStore.updateQuantityProduct(id, countPlus);
+  }
+  minumItem(id: number) {
+    const countMinum: number = -1;
+    this.cartStore.updateQuantityProduct(id, countMinum);
+  }
 }
--- src/app/layout/header/header.component.ts
+++ src/app/layout/header/header.component.ts
@@ -1,6 +1,7 @@
 import { CurrencyPipe, NgClass, SlicePipe } from '@angular/common';
 import { Component, inject, signal } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { CartComponent } from '@features/cart/cart/cart.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { faCartShopping, faShop } from '@fortawesome/free-solid-svg-icons';
 import { CartStoreSignal } from '@shared/store/shopping.cart.store';
@@ -8,7 +9,14 @@ import { CartStoreSignal } from '@shared/store/shopping.cart.store';
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [FontAwesomeModule, RouterLink, NgClass, CurrencyPipe, SlicePipe],
+  imports: [
+    CartComponent,
+    FontAwesomeModule,
+    RouterLink,
+    NgClass,
+    CurrencyPipe,
+    SlicePipe,
+  ],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css',
 })
