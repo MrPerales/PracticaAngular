251eb61e22ea5253aa3badb7b28f123eaa22ba2d
Carlos Perales <carlosperales050@gmail.com>
2025-01-14 18:36:56 -0600

    add : finders

---
+++ src/app/testing/finders.ts
@@ -0,0 +1,19 @@
+import { ComponentFixture } from '@angular/core/testing';
+import { By } from '@angular/platform-browser';
+
+export function query<T>(fixture: ComponentFixture<T>, selector: string) {
+  const debugElement = fixture.debugElement.query(By.css(selector));
+  if (!debugElement) {
+    return new Error(`query: element with ${selector} not found`);
+  }
+  return debugElement;
+}
+
+export function queryById<T>(fixture: ComponentFixture<T>, testId: string) {
+  const selector = `[data-testid="${testId}"]`;
+  return query(fixture, selector);
+}
+
+export function queryAll<T>(fixture: ComponentFixture<T>, selector: string) {
+  return fixture.debugElement.queryAll(By.css(selector));
+}
