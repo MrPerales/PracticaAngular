44ec8c4636f82b3a21a795d655c3dbc354eca60f
Carlos Perales <carlosperales050@gmail.com>
2025-01-31 16:10:49 -0600

    fix test for appComponent

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,29 +1,46 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { provideRouter } from '@angular/router';
+import { Component, NO_ERRORS_SCHEMA } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  template: '<div></div>',
+})
+export class headerComponentMock {}
+
+@Component({
+  selector: 'app-spinner',
+  standalone: true,
+  template: '<div></div>',
+})
+export class spinnerComponentMock {}
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let toastrService: jasmine.SpyObj<ToastrService>;
+
   beforeEach(async () => {
+    const toastrServiceSpy = jasmine.createSpyObj('ToastrService', ['success']);
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, headerComponentMock, spinnerComponentMock],
+      providers: [
+        provideRouter([]),
+        { provide: ToastrService, useValue: toastrServiceSpy },
+      ],
+      // schemas: [NO_ERRORS_SCHEMA], //para ignorar los warnings de los componentes no declarados
     }).compileComponents();
+    fixture = TestBed.createComponent(AppComponent);
+    toastrService = TestBed.inject(
+      ToastrService
+    ) as jasmine.SpyObj<ToastrService>;
+    component = fixture.componentInstance;
   });
 
   it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app).toBeTruthy();
-  });
-
-  it(`should have the 'storeAngular17' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('storeAngular17');
-  });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, storeAngular17');
+    expect(component).toBeTruthy();
   });
 });
